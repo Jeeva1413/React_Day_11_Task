69e62b00045e860c1d4578e3c2eba8a4e96e9fc5
Jeeva1413 <jeevacs1413@gmail.com>
2024-07-11 10:48:46 +0530

    update

---
+++ src/Components/SideBar.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { BookOutlined } from "@ant-design/icons";
+import { Link } from "react-router-dom";
+
+const SideBar = () => {
+  return (
+    <div className="logo">
+      <div className="logo-icon">
+        <Link to='/' ><BookOutlined /></Link>
+      </div>
+    </div>
+  );
+};
+
+export default SideBar;
